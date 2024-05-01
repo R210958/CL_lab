59bf10b64a6adfe1b457674e3b2b75d5a7952555
R210958 <168629383+R210958@users.noreply.github.com>
2024-05-01 14:36:26 +0530

    Add files via upload

---
+++ dotproduct.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# Define two vectors
+vector1 = [1, 2, 3]
+vector2 = [4, 5, 6]
+
+# Calculate the dot product
+dot_product = np.dot(vector1, vector2)
+
+# Print the result
+print("The dot product of the vectors is:", dot_product)
