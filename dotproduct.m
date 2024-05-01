0ba0ae6f127a7f71feb8c0162096b8cc609585ec
R210958 <168629383+R210958@users.noreply.github.com>
2024-05-01 14:35:23 +0530

    Add files via upload

--- dotproduct.m
+++ dotproduct.m
@@ -1,4 +1,10 @@
-%taking 20 array
-c=[2,3;3,0];
-d=[2,1;3,5];
-disp(d*c);
+% Taking two 2D arrays
+a = [2, 1; 0, 3];
+b = [1, 1; 3, 2];
+
+% Calculating dot product using matrix multiplication
+% Note that in Octave, matrix multiplication (*) represents the dot product
+% of corresponding rows and columns of the matrices
+% We need to transpose one matrix to ensure the dimensions are compatible
+% Here, I'm transposing matrix b to get the same output as the Python code
+disp(b' * a);
